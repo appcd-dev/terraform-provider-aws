7114ccd00f63d6aeeb583c108ec3cf32f6e85c9e
Sabith KS <sks@stackgen.com>
2025-02-25 08:07:43 -0800

    Keep changes minimal

---
+++ conns/export.go
@@ -0,0 +1,7 @@
+package conns
+
+import "github.com/hashicorp/terraform-provider-aws/internal/conns"
+
+type Config conns.Config
+
+type AWSClient conns.AWSClient
--- provider/export.go
+++ provider/export.go
@@ -6,33 +6,18 @@ package provider
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-provider-aws/internal/provider/fwprovider"
+	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 )
 
 // ProtoV5ProviderServerFactory returns a muxed terraform-plugin-go protocol v5 provider factory function.
 // This factory function is suitable for use with the terraform-plugin-go Serve function.
 // The primary (Plugin SDK) provider server is also returned (useful for testing).
 func ProtoV5ProviderServerFactory(ctx context.Context) (func() tfprotov5.ProviderServer, *schema.Provider, error) {
-	primary, err := New(ctx)
+	return provider.ProtoV5ProviderServerFactory(ctx)
+}
 
-	if err != nil {
-		return nil, nil, err
-	}
-
-	servers := []func() tfprotov5.ProviderServer{
-		primary.GRPCProvider,
-		providerserver.NewProtocol5(fwprovider.New(primary)),
-	}
-
-	muxServer, err := tf5muxserver.NewMuxServer(ctx, servers...)
-
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return muxServer.ProviderServer, primary, nil
+func New(ctx context.Context) (*schema.Provider, error) {
+	return provider.New(ctx)
 }
