a6a74d24b57c50085dd6c2d7b5c3c520fd58dbae
Sabith KS <sks@stackgen.com>
2025-02-26 07:18:47 -0800

    forgot these

--- conns/export.go
+++ conns/export.go
@@ -1,7 +1,21 @@
 package conns
 
-import "github.com/hashicorp/terraform-provider-aws/internal/conns"
+import (
+	"context"
+	"iter"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-provider-aws/internal/conns"
+)
 
 type Config conns.Config
 
 type AWSClient conns.AWSClient
+
+func (c *Config) ConfigureProvider(ctx context.Context, client *AWSClient) (*AWSClient, diag.Diagnostics) {
+	return conns.Config(c).ConfigureProvider(ctx)
+}
+
+func (c *AWSClient) ServicePackages(context.Context) iter.Seq2[string, ServicePackage] {
+	return conns.AWSClient(*c).ServicePackages(context.Background())
+}
