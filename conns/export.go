9611845179b9605295d605b687dcdbed2d348084
Sabith KS <sks@stackgen.com>
2025-02-26 09:41:52 -0800

    alias

--- conns/export.go
+++ conns/export.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"iter"
 
-	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 )
 
@@ -12,10 +12,11 @@ type Config conns.Config
 
 type AWSClient conns.AWSClient
 
-func (c *Config) ConfigureProvider(ctx context.Context) (*AWSClient, diag.Diagnostics) {
-	return conns.Config(c).ConfigureProvider(ctx)
+func (c *Config) ConfigureProvider(ctx context.Context, client *AWSClient) (*AWSClient, diag.Diagnostics) {
+	val, diag := (*conns.Config)(c).ConfigureProvider(ctx, (*conns.AWSClient)(client))
+	return (*AWSClient)(val), diag
 }
 
-func (c *AWSClient) ServicePackages(context.Context) iter.Seq2[string, ServicePackage] {
-	return conns.AWSClient(*c).ServicePackages(context.Background())
+func (c *AWSClient) ServicePackages(context.Context) iter.Seq2[string, conns.ServicePackage] {
+	return (*conns.AWSClient)(c).ServicePackages(context.Background())
 }
