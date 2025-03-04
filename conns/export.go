11a691534478f0afa6562684e15c98825b1c0ab1
Sabith KS <sks@stackgen.com>
2025-03-03 17:42:31 -0800

    add types

--- conns/export.go
+++ conns/export.go
@@ -3,6 +3,7 @@ package conns
 import (
 	"context"
 	"iter"
+	"maps"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
@@ -12,15 +13,27 @@ type Config conns.Config
 
 type AWSClient conns.AWSClient
 
+type ServicePackage conns.ServicePackage
+
 func (c *Config) ConfigureProvider(ctx context.Context, client *AWSClient) (*AWSClient, diag.Diagnostics) {
 	val, diag := (*conns.Config)(c).ConfigureProvider(ctx, (*conns.AWSClient)(client))
 	return (*AWSClient)(val), diag
 }
 
-func (c *AWSClient) SetServicePackages(ctx context.Context, packages map[string]conns.ServicePackage) {
+func (c *AWSClient) SetServicePackages(ctx context.Context, input map[string]ServicePackage) {
+	packages := make(map[string]conns.ServicePackage)
+	for k, v := range input {
+		packages[k] = conns.ServicePackage(v)
+	}
 	(*conns.AWSClient)(c).SetServicePackages(ctx, packages)
 }
 
-func (c *AWSClient) ServicePackages(ctx context.Context) iter.Seq2[string, conns.ServicePackage] {
-	return (*conns.AWSClient)(c).ServicePackages(ctx)
+func (c *AWSClient) ServicePackages(ctx context.Context) iter.Seq2[string, ServicePackage] {
+	servicePackage := (*conns.AWSClient)(c).ServicePackages(ctx)
+	result := make(map[string]ServicePackage)
+	servicePackage(func(k string, v conns.ServicePackage) bool {
+		result[k] = v
+		return true
+	})
+	return maps.All(result)
 }
