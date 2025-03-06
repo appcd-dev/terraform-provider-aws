ee5cfa613bf71ab9d508743e91b82274dbecea36
Sabith KS <sks@stackgen.com>
2025-03-05 22:43:47 -0800

    Duh

--- provider/provider.go
+++ provider/provider.go
@@ -7,10 +7,6 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/provider"
 )
 
-type Provider = provider.Provider
-
-type Resource = provider.Resource
-
 // New returns a new, initialized Terraform Plugin SDK v2-style provider instance.
 // The provider instance is fully configured once the `ConfigureContextFunc` has been called.
 func New(ctx context.Context) (*schema.Provider, error) {
