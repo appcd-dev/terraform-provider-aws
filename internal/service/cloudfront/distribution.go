cb81583b7d33241bac0ee6ef45004899a83f2ec0
Vishwajeet Kumar <vishwajeet@appcd.com>
2025-10-30 21:43:06 +0530

    address PR comment

--- internal/service/cloudfront/distribution.go
+++ internal/service/cloudfront/distribution.go
@@ -1496,7 +1496,7 @@ func flattenCacheBehavior(apiObject *awstypes.CacheBehavior) map[string]interfac
 		tfMap["forwarded_values"] = []interface{}{flattenForwardedValues(apiObject.ForwardedValues)}
 	}
 
-	if len(apiObject.FunctionAssociations.Items) > 0 {
+	if apiObject.FunctionAssociations != nil && len(apiObject.FunctionAssociations.Items) > 0 {
 		tfMap["function_association"] = flattenFunctionAssociations(apiObject.FunctionAssociations)
 	}
 
@@ -1504,7 +1504,7 @@ func flattenCacheBehavior(apiObject *awstypes.CacheBehavior) map[string]interfac
 		tfMap["grpc_config"] = []interface{}{flattenGRPCConfig(apiObject.GrpcConfig)}
 	}
 
-	if len(apiObject.LambdaFunctionAssociations.Items) > 0 {
+	if apiObject.LambdaFunctionAssociations != nil && len(apiObject.LambdaFunctionAssociations.Items) > 0 {
 		tfMap["lambda_function_association"] = flattenLambdaFunctionAssociations(apiObject.LambdaFunctionAssociations)
 	}
 
@@ -1520,11 +1520,11 @@ func flattenCacheBehavior(apiObject *awstypes.CacheBehavior) map[string]interfac
 		tfMap["smooth_streaming"] = aws.ToBool(apiObject.SmoothStreaming)
 	}
 
-	if len(apiObject.TrustedKeyGroups.Items) > 0 {
+	if apiObject.TrustedKeyGroups != nil && len(apiObject.TrustedKeyGroups.Items) > 0 {
 		tfMap["trusted_key_groups"] = flattenTrustedKeyGroups(apiObject.TrustedKeyGroups)
 	}
 
-	if len(apiObject.TrustedSigners.Items) > 0 {
+	if apiObject.TrustedSigners != nil && len(apiObject.TrustedSigners.Items) > 0 {
 		tfMap["trusted_signers"] = flattenTrustedSigners(apiObject.TrustedSigners)
 	}
 
